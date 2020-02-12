94d5b6682cbfadd4acc57b1ff0300013a5f7ddcc
Richard Pinheiro <richardpinheiro1992@gmail.com>
2020-02-12 00:26:30 -0200

    create mail sender

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+      user: "b301ce7c8bfa80",
+      pass: "c140cbf9c7d000"
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>'
+    }
+}
