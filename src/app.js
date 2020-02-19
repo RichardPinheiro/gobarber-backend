81dfac420abc1af3daef3e6a1761128814d34ee3
Richard Pinheiro <richardpinheiro1992@gmail.com>
2020-02-19 01:04:20 -0300

    added sentry to app for logs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import path from 'path'
+import Youch from 'youch'
+import * as Sentry from '@sentry/node'
+import 'express-async-errors'
+
 import routes from './routes/routes'
+import sentryConfig from './config/sentry'
 
 import './database'
 
@@ -8,8 +13,12 @@ class App {
     constructor() {
         this.server = express()
 
+        Sentry.init(sentryConfig)
+
+        this.server.use(Sentry.Handlers.requestHandler())
         this.middlewares()
         this.routes()
+        this.exceptionHandler()
     }
 
     middlewares() {
@@ -22,6 +31,15 @@ class App {
 
     routes() {
         this.server.use(routes)
+        this.server.use(Sentry.Handlers.errorHandler())
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON()
+
+            return res.status(500).json(errors)
+        })
     }
 }
 
